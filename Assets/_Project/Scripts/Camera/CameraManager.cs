31ab45de1b0f264c5264481570ce26f08676b195
Nytrock <savvazhirnov@gmail.com>
2025-02-09 16:27:45 +0300

    Fix camera glitch

--- Assets/_Project/Scripts/Camera/CameraManager.cs
+++ Assets/_Project/Scripts/Camera/CameraManager.cs
@@ -50,7 +50,7 @@ public class CameraManager : MonoBehaviour {
 
         if (Input.GetMouseButton(0)) {
             Vector3 direction = _touchOffset - LocalMousePosition;
-            transform.position = direction;
+            transform.position = new(direction.x, direction.y, -10);
         }
     }
 
